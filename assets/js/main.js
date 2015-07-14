fe4ef8a6fef0fdf0f65596a80ba6d8c610c064f1
Ahmed Albarzangi <Ahmed@Ahmeds-MacBook-Pro.fritz.box>
2015-07-14 15:10:47 +0200

    First Commit

---
+++ assets/js/main.js
@@ -0,0 +1,123 @@
+/*
+	Strongly Typed by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+var api_url = "";
+
+var app = angular.module('ITLounge', []);
+
+app.controller('laadQuotes', function($scope, $http) {
+
+	$http({
+		method: 'GET',
+		url: 'test.php'
+	}).success(function(data, status) {
+
+		//console.log(data);
+		$scope.quotes = angular.fromJson(data);
+		//console.log($scope.quotes);
+	}).error(function(data, status) {
+		// Some error occurred
+	});
+
+	$scope.formatDate = function(date){
+		var date = date.split("-").join("/");
+		var dateOut = new Date(date);
+		return dateOut;
+	};
+
+});
+
+
+
+(function($) {
+
+	skel
+		.breakpoints({
+			desktop: '(min-width: 737px)',
+			tablet: '(min-width: 737px) and (max-width: 1200px)',
+			mobile: '(max-width: 736px)'
+		})
+		.viewport({
+			breakpoints: {
+				tablet: {
+					width: 1080
+				}
+			}
+		});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				$body.removeClass('is-loading');
+			});
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on mobile.
+			skel.on('+mobile -mobile', function() {
+				$.prioritize(
+					'.important\\28 mobile\\29',
+					skel.breakpoint('mobile').active
+				);
+			});
+
+		// CSS polyfills (IE<9).
+			if (skel.vars.IEVersion < 9)
+				$(':last-child').addClass('last-child');
+
+		// Dropdowns.
+			$('#nav > ul').dropotron({
+				mode: 'fade',
+				noOpenerFade: true,
+				hoverDelay: 150,
+				hideDelay: 350
+			});
+
+		// Off-Canvas Navigation.
+
+			// Title Bar.
+				$(
+					'<div id="titleBar">' +
+						'<a href="#navPanel" class="toggle"></a>' +
+					'</div>'
+				)
+					.appendTo($body);
+
+			// Navigation Panel.
+				$(
+					'<div id="navPanel">' +
+						'<nav>' +
+							$('#nav').navList() +
+						'</nav>' +
+					'</div>'
+				)
+					.appendTo($body)
+					.panel({
+						delay: 500,
+						hideOnClick: true,
+						hideOnSwipe: true,
+						resetScroll: true,
+						resetForms: true,
+						side: 'left',
+						target: $body,
+						visibleClass: 'navPanel-visible'
+					});
+
+			// Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
+				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
+					$('#titleBar, #navPanel, #page-wrapper')
+						.css('transition', 'none');
+
+	});
+
+})(jQuery);
---
+++ test.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ahmed
+ * Date: 13/07/15
+ * Time: 22:28
+ */
+header('Content-Type: application/json');
+
+$quotes_array = array("quotes"=> array(
+                                array("naam"=>"X", "Quote"=>"Ik ben Wijnand", "date"=>date("Y-m-d H:i:s")),
+                                array("naam"=>"TC", "Quote"=>"Ik ben Karlo", "date"=>date("Y-m-d H:i:s")),
+                                array("naam"=>"TT", "Quote"=>"Ik ben Thomas", "date"=>date("Y-m-d H:i:s")),
+    array("naam"=>"X", "Quote"=>"Ik ben Wijnand", "date"=>date("Y-m-d H:i:s")),
+    array("naam"=>"TC", "Quote"=>"Ik ben Karlo", "date"=>date("Y-m-d H:i:s")),
+    array("naam"=>"TT", "Quote"=>"Ik ben Thomas", "date"=>date("Y-m-d H:i:s")),
+    array("naam"=>"X", "Quote"=>"Ik ben Wijnand", "date"=>date("Y-m-d H:i:s")),
+    array("naam"=>"TC", "Quote"=>"Ik ben Karlo", "date"=>date("Y-m-d H:i:s")),
+    array("naam"=>"TT", "Quote"=>"Ik ben Thomas", "date"=>date("Y-m-d H:i:s"))
+
+
+)
+);
+
+echo file_get_contents("http://8455218b.ngrok.io/quotes");
+//echo json_encode($quotes_array);
+
+?>
